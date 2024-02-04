c7c0063f69eddf2f308c369627c4d7dbd81a7d93
juice2227 <myceline2227.gmail.com>
2024-02-04 13:53:24 +0300

    weather-app

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
 
 export default function Weather(){
     const [city, setCity] = useState('')
+    const [weatherData, setweatherData] = useState(null)
+    //handle input onchange
     const handleInputChange =(e)=>{
         setCity(e.target.city)
 
     }
     //fetchData
     const fetchData = () => {
-        //fetch from api
-    const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={YOUR_API_KEY}`)
+        //fetch from apire  
+    const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={ad94a9611fef414000cb6b3b4fedc9cc}`)
+    setweatherData(response.data)
+    console.log(weatherData)
 
+    }
+    useEffect{()=>{
+        fetchData()
+    }.[]
+    
     }
     //handleSubmit
     const handleSubmit =(e) =>{
